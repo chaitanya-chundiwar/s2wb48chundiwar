7a444689a6cc7888880235129a7862e4d8e6b71f
chaitanya-chundiwar <chaitanya833krishna@gmail.com>
2022-04-06 21:00:32 -0500

    submotteed

--- README.md
+++ README.md
@@ -2,6 +2,5 @@
 Learning Pug
 
 [Server at](https://s2wb48chundiwar.herokuapp.com/)
-ClassName: beverages
-Attributes: Beverages_name, Beverages_Bottle_Size, Beverages_cost
-My class beverages with attributes beverages_name (string), beverages_bottle_Size (string), and beverages_cost (number).
+
+My class beverages with attributes beverages_name (string), beverages_Size (string), and beverages_cost (number).
--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var beveragesRouter = require('./routes/beverages');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 var app = express();
 
 // view engine setup
@@ -21,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/beverages', beveragesRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
